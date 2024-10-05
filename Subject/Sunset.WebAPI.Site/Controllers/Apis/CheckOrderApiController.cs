b4a1cd0a6607bac214051707e3921e184f2919d7
Leo Chiu <zu00804848@gmail.com>
2024-10-05 17:55:45 +0800

    10/5課堂coding

--- Subject/Sunset.WebAPI.Site/Controllers/Apis/CheckOrderApiController.cs
+++ Subject/Sunset.WebAPI.Site/Controllers/Apis/CheckOrderApiController.cs
@@ -1,21 +1,26 @@
 ﻿using Sunset.WebAPI.Site.Models.EFModels;
 using Sunset.WebAPI.Site.Models.Services;
+using Sunset.WebAPI.Site.Repositories;
+using Sunset.WebAPI.Site.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Security;
 
 namespace Sunset.WebAPI.Site.Controllers.Apis
 {
     public class CheckOrderApiController : ApiController
     {
         private readonly BookTicketsService _service;
+        private readonly UserHistoryService _userHistoryService;
 
         public CheckOrderApiController()
         {
             _service = new BookTicketsService();
+            _userHistoryService = new UserHistoryService(new UserHistoryRepository(new AppDbContext()));
         }
         [HttpGet]
         [Route("api/CheckOrderApi/GetMovieScheduleId/{movieId}/{showdateId}/{showtimeId}")]
@@ -54,5 +59,54 @@ namespace Sunset.WebAPI.Site.Controllers.Apis
             return Ok(checkOrder);
         }
 
+        [HttpGet]
+        [Route("api/CheckOrderApi/CheckOrder/")]
+        public IHttpActionResult CheckCurrentOrder()
+        {
+            var authHeader = Request.Headers.Authorization;
+            if (authHeader == null || authHeader.Scheme != "Bearer")
+            {
+                return Unauthorized();
+            }
+
+            var token = authHeader.Parameter;
+            var memberId = GetMemberIdFromToken(token);
+
+            if (string.IsNullOrEmpty(memberId))
+            {
+                return Unauthorized();
+            }
+
+            var (topupHistory, currentBalance) = _transactionHistoryService.GetTopupHistoryByMemberId(memberId);
+
+            if (topupHistory == null || !topupHistory.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(new { CurrentBalance = currentBalance, TopupHistory = topupHistory });
+        }
+
+
+        private string GetMemberIdFromToken(string token)
+        {
+            try
+            {
+                var ticket = FormsAuthentication.Decrypt(token);
+                if (ticket == null)
+                {
+                    return null;
+                }
+
+                var email = ticket.Name;
+                var member = _userHistoryService.GetMemberByEmail(email);
+                return member?.Id.ToString();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error decoding token: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
